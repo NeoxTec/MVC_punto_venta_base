dd85015133c72829bdc2dceec04b781632e0e18a
MarioAlbertoNL1799 <37378931+MarioAlbertoNL1799@users.noreply.github.com>
2018-11-20 00:25:13 -0600

    0.- Se crean los metodos que habilitan y deshabilitan los campos del catalogo asi como los botones de interaccion con la base de datos. 1.- Se crea el evento mouseclicked del metodo guardar el cual evalua para poder ingresar o actualizar un dato.

--- src/controllers/controllerCatalogo.java
+++ src/controllers/controllerCatalogo.java
@@ -8,6 +8,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.JOptionPane;
 import views.viewCatalogo;
 import models.modelCatalogo;
 /**
@@ -22,6 +23,11 @@ public class controllerCatalogo {
         this.modelCatalogo = modelCatalogo;
         this.viewCatalogo = viewCatalogo;
         this.viewCatalogo.jt_catalogo.addMouseListener(ml);
+        this.viewCatalogo.jb_nuevo.addMouseListener(ml);
+        this.viewCatalogo.jb_modificar.addMouseListener(ml);
+        this.viewCatalogo.jb_guardar.addMouseListener(ml);
+        this.viewCatalogo.jb_eliminar.addMouseListener(ml);
+        this.viewCatalogo.jb_cancelar.addMouseListener(ml);
         this.viewCatalogo.jtf_buscar.addKeyListener(kl);
         initDB();
         viewCatalogo.setVisible(true);
@@ -32,12 +38,33 @@ public class controllerCatalogo {
         modelCatalogo.setSentencia("SELECT id,nombre,precio, precio_mayoreo,unidad FROM catalogo");
         modelCatalogo.llenartabla();
         viewCatalogo.jt_catalogo.setModel(modelCatalogo.getModelo());
+        habilitar(false);
+        botones_p(true);
     }
      MouseListener ml = new MouseListener(){
         @Override
         public void mouseClicked(MouseEvent e) {
             if(e.getSource()== viewCatalogo.jt_catalogo){
                 jt_catalogo_mouseClicked();
+            }
+            else if(e.getSource()== viewCatalogo.jb_nuevo){
+                botones_p(false);
+                habilitar(true);
+                modelCatalogo.setId(0);
+                limpiar_campos();
+                System.out.println(modelCatalogo.getId());
+            }
+            else if(e.getSource() == viewCatalogo.jb_modificar){
+                botones_p(false);
+                habilitar(true);
+            }
+            else if(e.getSource()==viewCatalogo.jb_cancelar){
+            botones_p(true);
+            habilitar(false);
+            limpiar_campos();
+            }
+            else if(e.getSource()==viewCatalogo.jb_guardar){
+            jb_guardar_mouseClicked();
             }
         }
 
@@ -88,11 +115,63 @@ public class controllerCatalogo {
          modelCatalogo.llenartabla();
          viewCatalogo.jt_catalogo.setModel(modelCatalogo.getModelo());
      }
+     public void habilitar(boolean f){
+     viewCatalogo.jta_descripcion.setEditable(f);
+     viewCatalogo.jtf_codigobarras.setEditable(f);
+     viewCatalogo.jtf_nombre.setEditable(f);
+     viewCatalogo.jtf_preciom.setEditable(f);
+     viewCatalogo.jtf_preciou.setEditable(f);
+     viewCatalogo.jcb_iva.setEnabled(f);
+     viewCatalogo.jcb_unidadmedida.setEnabled(f);
+     viewCatalogo.jb_guardar.setEnabled(f);
+     viewCatalogo.jb_cancelar.setEnabled(f);
+     }
+     public void botones_p(boolean p){
+     viewCatalogo.jb_nuevo.setEnabled(p);
+     viewCatalogo.jb_eliminar.setEnabled(p);
+     viewCatalogo.jb_modificar.setEnabled(p);
+     }
      public void limpiar(){
           for (int i = 0; i < viewCatalogo.jt_catalogo.getRowCount(); i++) {
             modelCatalogo.getModelo().removeRow(i);
             i -= 1;
         }
      }
-
+     public void limpiar_campos(){
+     viewCatalogo.jta_descripcion.setText(null);
+     viewCatalogo.jtf_codigobarras.setText(null);
+     viewCatalogo.jtf_nombre.setText(null);
+     viewCatalogo.jtf_preciom.setText(null);
+     viewCatalogo.jtf_preciou.setText(null);
+     viewCatalogo.jcb_iva.setSelectedIndex(0);
+     viewCatalogo.jcb_unidadmedida.setSelectedIndex(0);
+     }
+     
+     public void jb_guardar_mouseClicked(){
+         if (viewCatalogo.jtf_codigobarras.getText().isEmpty())
+             modelCatalogo.setCodigo_barras(0);
+         else
+                modelCatalogo.setCodigo_barras(Integer.parseInt(viewCatalogo.jtf_codigobarras.getText()));
+         if (viewCatalogo.jtf_preciom.getText().isEmpty())
+                 modelCatalogo.setPrecio_mayoreo(0);
+         else
+                  modelCatalogo.setPrecio_mayoreo(Double.parseDouble(viewCatalogo.jtf_preciom.getText()));
+         if (viewCatalogo.jtf_nombre.getText().isEmpty() || viewCatalogo.jta_descripcion.getText().isEmpty() || viewCatalogo.jtf_preciou.getText().isEmpty())
+              JOptionPane.showMessageDialog(null, "Ingrese valores en nombre, descripcion y precio unitario como minimo para poder agregar producto");
+         else{
+         modelCatalogo.setDescripcion(viewCatalogo.jta_descripcion.getText());
+         modelCatalogo.setIva(viewCatalogo.jcb_iva.getSelectedItem());
+         modelCatalogo.setNombre(viewCatalogo.jtf_nombre.getText());
+         modelCatalogo.setPrecio_unitario(Double.parseDouble(viewCatalogo.jtf_preciou.getText()));
+         modelCatalogo.setUnidad_medida(viewCatalogo.jcb_unidadmedida.getSelectedItem());
+         if (modelCatalogo.getId() > 0)
+              modelCatalogo.modificar();
+          else
+              modelCatalogo.insertar();
+         }
+          botones_p(true);
+          habilitar(false);
+          limpiar();
+          initDB();
+     }
  }
