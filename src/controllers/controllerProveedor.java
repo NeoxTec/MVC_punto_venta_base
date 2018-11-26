b6ce07993c95a8f35611e41562062753949fdc3a
Abigailnm <1717110627@utectulancingo.edu.mx>
2018-11-26 12:37:59 -0600

    correccion de error

--- src/controllers/controllerProveedor.java
+++ src/controllers/controllerProveedor.java
@@ -15,27 +15,28 @@ import views.viewProveedor;
  * @author Abi Montes
  */
 public class controllerProveedor {
-    modelProveedor ModelProveedor;
+    modelProveedor modelProveedor;
     viewProveedor viewProveedor;  
     public controllerProveedor(modelProveedor modelProveedor, viewProveedor viewProveedor) {
-        this.ModelProveedor = modelProveedor;
+        this.modelProveedor = modelProveedor;
         this.viewProveedor = viewProveedor;
         setActionlistener();
         
         initComponents();
+        initDB();
 }
  public void initComponents() {
 
         viewProveedor.jtf_rfc.setEditable(true);
         viewProveedor.jtf_razons.setEditable(true);
-        viewProveedor.jtf_calle.setEditable(false);
-        viewProveedor.jtf_calle.setEditable(false);
-        viewProveedor.jtf_no_ext.setEditable(false);
-        viewProveedor.jtf_cp.setEditable(false);
-        viewProveedor.jtf_telefono.setEditable(false);
-        viewProveedor.jtf_correo.setEditable(false);
-        viewProveedor.jb_cancelar.setEnabled(false);
-        viewProveedor.jb_guardar.setEnabled(false);
+        viewProveedor.jtf_calle.setEditable(true);
+        viewProveedor.jtf_colonia.setEditable(true);
+        viewProveedor.jtf_no_ext.setEditable(true);
+        viewProveedor.jtf_cp.setEditable(true);
+        viewProveedor.jtf_telefono.setEditable(true);
+        viewProveedor.jtf_correo.setEditable(true);
+       // viewProveedor.jb_modificar.setEnabled(false);
+       // viewProveedor.jb_eliminar.setEnabled(false);
  }
 
 public void setActionlistener() {
@@ -44,14 +45,24 @@ public void setActionlistener() {
         viewProveedor.jb_agregar.addActionListener(actionListener);
         viewProveedor.jb_guardar.addActionListener(actionListener);
         viewProveedor.jb_cancelar.addActionListener(actionListener);
-        
-
-    }
+         }
+public void initDB(){
+    modelProveedor.conectarDB();
+    
+    viewProveedor.jtf_rfc.setText(modelProveedor.getRfc());
+    viewProveedor.jtf_razons.setText(modelProveedor.getRazons());
+     viewProveedor.jtf_calle.setText(modelProveedor.getCalle());
+      viewProveedor.jtf_colonia.setText(modelProveedor.getColonia());
+       viewProveedor.jtf_no_ext.setText(modelProveedor.getNoext());
+        viewProveedor.jtf_cp.setText(modelProveedor.getCp());
+         viewProveedor.jtf_telefono.setText(modelProveedor.getTelefono());
+          viewProveedor.jtf_correo.setText(modelProveedor.getCorreo());
+}
     ActionListener actionListener = new ActionListener() {
         @Override        public void actionPerformed(ActionEvent e) {
             if (e.getSource() == viewProveedor.jb_agregar) {
 
-             jb_insertar_actionPerformed();
+            jb_nuevo_actionPerformed();
 
             } else if (e.getSource() == viewProveedor.jb_modificar) {
 
@@ -63,63 +74,92 @@ public void setActionlistener() {
 
             } else if (e.getSource() == viewProveedor.jb_guardar) {
 
-               jb_nuevo_actionPerformed();
                
+               jb_insertar_actionPerformed();
                
                
 
             }
+            else if (e.getSource() == viewProveedor.jb_cancelar) {
+
+               
+               
+               jb_cancelar_actionPerformed();
+               
+
+            }
+            
         }
     };
     
 
     public void jb_nuevo_actionPerformed() {
         viewProveedor.jtf_rfc.setText(""); 
+        viewProveedor.jtf_razons.setText("");
         viewProveedor.jtf_calle.setText("");
         viewProveedor.jtf_colonia.setText("");
         viewProveedor.jtf_no_ext.setText("");
-        viewProveedor.jtf_razons.setText("");
+        
         viewProveedor.jtf_cp.setText("");
         viewProveedor.jtf_telefono.setText("");
         viewProveedor.jtf_correo.setText("");
+        viewProveedor.jb_modificar.setEnabled(false);
+       viewProveedor.jb_eliminar.setEnabled(false);
     }
     /**
      * Elimina un registro de la tabla contactos
      */
     public void jb_eliminar_actionPerformed() {
-    ModelProveedor.eliRegistro(); // Invoca metodo de eliminar 
+    modelProveedor.eliRegistro(); // Invoca metodo de eliminar 
+    viewProveedor.jb_modificar.setEnabled(false);
+       viewProveedor.jb_eliminar.setEnabled(false);
+       viewProveedor.jb_agregar.setEnabled(false);
+       modelProveedor.modiRegistro();
     }
  /**
      *Inserta registro en la tabla contactos
      */
     public void jb_insertar_actionPerformed() {
-        ModelProveedor.setRfc(viewProveedor.jtf_rfc.getText()); // Da el valor de nombre a la variable .
-        ModelProveedor.setNombre(viewProveedor.jtf_calle.getText()); 
-        ModelProveedor.setCalle(viewProveedor.jtf_colonia.getText());
-        ModelProveedor.setColonia(viewProveedor.jtf_no_ext.getText());
-        ModelProveedor.setNoext(viewProveedor.jtf_razons.getText()); 
-        ModelProveedor.setCp(viewProveedor.jtf_cp.getText()); 
-        ModelProveedor.setTelefono(viewProveedor.jtf_telefono.getText()); 
-        ModelProveedor.setCorreo(viewProveedor.jtf_correo.getText()); 
+        modelProveedor.setRfc(viewProveedor.jtf_rfc.getText()); // Da el valor de nombre a la variable .
+        modelProveedor.setCalle(viewProveedor.jtf_calle.getText()); 
+        modelProveedor.setColonia(viewProveedor.jtf_colonia.getText());
+        modelProveedor.setNoext(viewProveedor.jtf_no_ext.getText());
+        modelProveedor.setRazons(viewProveedor.jtf_razons.getText()); 
+        modelProveedor.setCp(viewProveedor.jtf_cp.getText()); 
+        modelProveedor.setTelefono(viewProveedor.jtf_telefono.getText()); 
+        modelProveedor.setCorreo(viewProveedor.jtf_correo.getText()); 
         
-        
-        
-        ModelProveedor.insertarRegistro(); // Invoca  Insertar un nuevo registro.
+      //  viewProveedor.jb_agregar.setEnabled(false);
+       // viewProveedor.jb_modificar.setEnabled(false);
+      // viewProveedor.jb_eliminar.setEnabled(false);
+       
+        modelProveedor.insertarRegistro(); // Invoca  Insertar un nuevo registro.
     }   
     /**
      * Método para modificar registros
      */
     public void jb_modificar_actionPerformed() {
 
-           ModelProveedor.setRfc(viewProveedor.jtf_rfc.getText()); // Da el valor de nombre a la variable .
-        ModelProveedor.setNombre(viewProveedor.jtf_calle.getText()); 
-        ModelProveedor.setCalle(viewProveedor.jtf_colonia.getText());
-        ModelProveedor.setColonia(viewProveedor.jtf_no_ext.getText());
-        ModelProveedor.setNoext(viewProveedor.jtf_razons.getText()); 
-        ModelProveedor.setCp(viewProveedor.jtf_cp.getText()); 
-        ModelProveedor.setTelefono(viewProveedor.jtf_telefono.getText()); 
-        ModelProveedor.setCorreo(viewProveedor.jtf_correo.getText()); 
+        modelProveedor.setRfc(viewProveedor.jtf_rfc.getText()); // Da el valor de nombre a la variable .
+        modelProveedor.setCalle(viewProveedor.jtf_calle.getText()); 
+        modelProveedor.setColonia(viewProveedor.jtf_colonia.getText());
+        modelProveedor.setNoext(viewProveedor.jtf_no_ext.getText());
+        modelProveedor.setRazons(viewProveedor.jtf_razons.getText()); 
+        modelProveedor.setCp(viewProveedor.jtf_cp.getText()); 
+        modelProveedor.setTelefono(viewProveedor.jtf_telefono.getText()); 
+        modelProveedor.setCorreo(viewProveedor.jtf_correo.getText()); 
         
-        ModelProveedor.modiRegistro(); // Invoca metodo de modificar .
+        
+        viewProveedor.jb_agregar.setEnabled(false);
+        
+       viewProveedor.jb_eliminar.setEnabled(false);
+        modelProveedor.modiRegistro(); // Invoca metodo de modificar .
+    }
+    public void jb_cancelar_actionPerformed() {
+        System.out.println("registro cancelado");
+        viewProveedor.jb_agregar.setEnabled(true);
+        viewProveedor.jb_modificar.setEnabled(true);
+       viewProveedor.jb_eliminar.setEnabled(true);
+       viewProveedor.jb_guardar.setEnabled(true);
     }
 }
