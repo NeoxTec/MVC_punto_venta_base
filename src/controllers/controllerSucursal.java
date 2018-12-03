11c176f7107c973ccb240a8063fe6211749c7a41
MarioAlbertoNL1799 <37378931+MarioAlbertoNL1799@users.noreply.github.com>
2018-12-03 12:17:38 -0600

    Controller Sucursal permite hacer busquedas dinamicas con los registros existentes y boton guardar permiteguardar cambios y guardar nuevos registros

--- src/controllers/controllerSucursal.java
+++ src/controllers/controllerSucursal.java
@@ -7,10 +7,13 @@ package controllers;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import models.modelSucursal;
 import views.viewSucursal;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.JOptionPane;
 /**
  *
  * @author abi
@@ -22,6 +25,8 @@ public class controllerSucursal {
         this.modelSucursal = modelSucursal;
         this.viewSucursal = viewSucursal;
         this.viewSucursal.jt_sucursales.addMouseListener(ml);
+        this.viewSucursal.jtf_buscar.addKeyListener(kl);
+        viewSucursal.jtf_id.setEditable(false);
         setActionlistener();       
         initComponents();
 }
@@ -30,13 +35,12 @@ public class controllerSucursal {
         modelSucursal.setSentencia("select * from sucursal;");
         modelSucursal.llenartabla();
         viewSucursal.jt_sucursales.setModel(modelSucursal.getT_sucursal());
-        viewSucursal.jtf_id.setEditable(false);
-        viewSucursal.jtf_calle.setEditable(false);
-        viewSucursal.jtf_colonia.setEditable(false);
-        viewSucursal.jtf_noexterior.setEditable(false);
-        viewSucursal.jtf_nointerior.setEditable(false);
-        viewSucursal.jtf_cp.setEditable(false);
-        viewSucursal.jtf_telefono.setEditable(false);
+        viewSucursal.jb_agregar.setEnabled(true);
+        viewSucursal.jb_modificar.setEnabled(true);
+        viewSucursal.jb_eliminar.setEnabled(true);
+        viewSucursal.jb_guardar.setEnabled(false);
+        viewSucursal.jb_cancelar.setEnabled(false);
+        celdas(false);
  }
 
 public void setActionlistener() {
@@ -63,7 +67,7 @@ public void setActionlistener() {
             } else if (e.getSource() == viewSucursal.jb_guardar) {
 
                
-               jb_insertar_actionPerformed();             
+               jb_guardar_actionPerformed();             
 
             }
             else if (e.getSource() == viewSucursal.jb_cancelar) {
@@ -77,7 +81,22 @@ public void setActionlistener() {
         }
     };
 
-    
+     /**
+      * Constructor que realiza las acciones del buscador
+      */
+     KeyListener kl = new KeyListener(){
+         @Override
+        public void keyTyped(KeyEvent e) {
+        }
+        @Override
+        public void keyPressed(KeyEvent e) {
+            jb_buscar_keypresseed();
+        }
+        @Override
+        public void keyReleased(KeyEvent e) {
+        }
+     };
+     
    MouseListener ml = new MouseListener(){
         @Override
         public void mouseClicked(MouseEvent e) {
@@ -99,8 +118,73 @@ public void setActionlistener() {
         public void mouseExited(MouseEvent e) { }
     };
     public void jb_nuevo_actionPerformed() {
+        celdas(true);
         viewSucursal.jtf_id.setText(""); 
+        viewSucursal.jtf_calle.setText("");
+        viewSucursal.jtf_colonia.setText("");
+        viewSucursal.jtf_noexterior.setText("");
+        viewSucursal.jtf_nointerior.setText("");
+        viewSucursal.jtf_cp.setText("");
+        viewSucursal.jtf_telefono.setText("");
+        modelSucursal.setId(0);
         
+        viewSucursal.jb_modificar.setEnabled(false);
+       viewSucursal.jb_eliminar.setEnabled(false);
+       viewSucursal.jb_agregar.setEnabled(false);
+       viewSucursal.jb_guardar.setEnabled(true);
+       viewSucursal.jb_cancelar.setEnabled(true);
+    }
+    /**
+     * Elimina un registro de la tabla contactos
+     */
+    public void jb_eliminar_actionPerformed() {
+    modelSucursal.eliRegistro(); // Invoca metodo de eliminar 
+        celdas(false);
+       viewSucursal.jb_modificar.setEnabled(true);
+       viewSucursal.jb_eliminar.setEnabled(true);
+       viewSucursal.jb_agregar.setEnabled(true);
+       viewSucursal.jb_cancelar.setEnabled(false);
+       viewSucursal.jb_guardar.setEnabled(false);
+    }
+ /**
+     *Inserta registro en la tabla contactos
+     */
+    public void jb_guardar_actionPerformed() {
+        if(viewSucursal.jtf_nointerior.getText().isEmpty())
+            modelSucursal.setNointerior(0);
+        if(viewSucursal.jtf_telefono.getText().isEmpty())
+            modelSucursal.setTelefono("Sin numero telefónico");
+        if(viewSucursal.jtf_calle.getText().isEmpty() || viewSucursal.jtf_colonia.getText().isEmpty() || viewSucursal.jtf_cp.getText().isEmpty() || viewSucursal.jtf_noexterior.getText().isEmpty())
+             JOptionPane.showMessageDialog(null, "Ingrese valores de calle, colonia, código postal, y número exterior para poder registrar la sucursal");
+        else{
+        modelSucursal.setCalle(viewSucursal.jtf_calle.getText()); 
+        modelSucursal.setColonia(viewSucursal.jtf_colonia.getText());
+        modelSucursal.setNoexterior(Integer.parseInt(viewSucursal.jtf_noexterior.getText()));
+        modelSucursal.setNointerior(Integer.parseInt(viewSucursal.jtf_nointerior.getText())); 
+        modelSucursal.setCp(viewSucursal.jtf_cp.getText()); 
+        modelSucursal.setTelefono(viewSucursal.jtf_telefono.getText()); 
+        if (modelSucursal.getId() == 0)
+            modelSucursal.insertarRegistro(); // Invoca  Insertar un nuevo registro. 
+        else
+            modelSucursal.modiRegistro(); // Invoca metodo de modificar .
+         limpiar();
+         initComponents();
+    }}   
+    /**
+     * Método para modificar registros
+     */
+    public void jb_modificar_actionPerformed() {     
+        celdas(true);
+        viewSucursal.jb_agregar.setEnabled(false);
+        viewSucursal.jb_modificar.setEnabled(false);
+       viewSucursal.jb_eliminar.setEnabled(false);
+       viewSucursal.jb_cancelar.setEnabled(true);
+       viewSucursal.jb_guardar.setEnabled(true);
+    }
+    public void jb_cancelar_actionPerformed() {
+        System.out.println("registro cancelado");
+        celdas(false);
+        viewSucursal.jtf_id.setText(""); 
         viewSucursal.jtf_calle.setText("");
         viewSucursal.jtf_colonia.setText("");
         viewSucursal.jtf_noexterior.setText("");
@@ -108,65 +192,11 @@ public void setActionlistener() {
         viewSucursal.jtf_cp.setText("");
         viewSucursal.jtf_telefono.setText("");
         
-        viewSucursal.jb_modificar.setEnabled(false);
-       viewSucursal.jb_eliminar.setEnabled(false);
-       viewSucursal.jb_agregar.setEnabled(false);
-    }
-    /**
-     * Elimina un registro de la tabla contactos
-     */
-    public void jb_eliminar_actionPerformed() {
-    modelSucursal.eliRegistro(); // Invoca metodo de eliminar 
-    viewSucursal.jb_modificar.setEnabled(false);
-       viewSucursal.jb_eliminar.setEnabled(false);
-       viewSucursal.jb_agregar.setEnabled(false);
-       modelSucursal.modiRegistro();
-    }
- /**
-     *Inserta registro en la tabla contactos
-     */
-    public void jb_insertar_actionPerformed() {
-        modelSucursal.setId(Integer.parseInt(viewSucursal.jtf_id.getText())); // Da el valor de nombre a la variable .
-        modelSucursal.setCalle(viewSucursal.jtf_calle.getText()); 
-        modelSucursal.setColonia(viewSucursal.jtf_colonia.getText());
-        modelSucursal.setNoexterior(Integer.parseInt(viewSucursal.jtf_noexterior.getText()));
-        modelSucursal.setNointerior(Integer.parseInt(viewSucursal.jtf_nointerior.getText())); 
-        modelSucursal.setCp(viewSucursal.jtf_cp.getText()); 
-        modelSucursal.setTelefono(viewSucursal.jtf_telefono.getText()); 
-        
-        
-      //  viewProveedor.jb_agregar.setEnabled(false);
-       // viewProveedor.jb_modificar.setEnabled(false);
-      // viewProveedor.jb_eliminar.setEnabled(false);
-       
-        modelSucursal.insertarRegistro(); // Invoca  Insertar un nuevo registro.
-    }   
-    /**
-     * Método para modificar registros
-     */
-    public void jb_modificar_actionPerformed() {
-
-        modelSucursal.setId(Integer.parseInt(viewSucursal.jtf_id.getText())); // Da el valor de nombre a la variable .
-        modelSucursal.setCalle(viewSucursal.jtf_calle.getText()); 
-        modelSucursal.setColonia(viewSucursal.jtf_colonia.getText());
-        modelSucursal.setNoexterior(Integer.parseInt(viewSucursal.jtf_noexterior.getText()));
-        modelSucursal.setNointerior(Integer.parseInt(viewSucursal.jtf_nointerior.getText())); 
-        modelSucursal.setCp(viewSucursal.jtf_cp.getText()); 
-        modelSucursal.setTelefono(viewSucursal.jtf_telefono.getText()); 
-        
-        
-        
-        viewSucursal.jb_agregar.setEnabled(false);
-        
-       viewSucursal.jb_eliminar.setEnabled(false);
-        modelSucursal.modiRegistro(); // Invoca metodo de modificar .
-    }
-    public void jb_cancelar_actionPerformed() {
-        System.out.println("registro cancelado");
-        viewSucursal.jb_agregar.setEnabled(true);
+       viewSucursal.jb_agregar.setEnabled(true);
         viewSucursal.jb_modificar.setEnabled(true);
        viewSucursal.jb_eliminar.setEnabled(true);
-       viewSucursal.jb_guardar.setEnabled(true);
+       viewSucursal.jb_guardar.setEnabled(false);
+       viewSucursal.jb_cancelar.setEnabled(false);
     }
     
       /**
@@ -185,4 +215,34 @@ public void setActionlistener() {
           viewSucursal.jtf_nointerior.setText(String.valueOf(modelSucursal.getNointerior()));
           viewSucursal.jtf_telefono.setText(modelSucursal.getTelefono());
 }
+     public void celdas(boolean i){
+         viewSucursal.jtf_calle.setEditable(i);
+         viewSucursal.jtf_colonia.setEditable(i);
+         viewSucursal.jtf_cp.setEditable(i);
+         viewSucursal.jtf_noexterior.setEditable(i);
+         viewSucursal.jtf_nointerior.setEditable(i);
+         viewSucursal.jtf_telefono.setEditable(i);
+     }
+     
+      /**
+      * Metodo que elimina los registros de la tabla de catalogo.
+      */
+     public void limpiar(){
+          for (int i = 0; i < viewSucursal.jt_sucursales.getRowCount(); i++) {
+            modelSucursal.getT_sucursal().removeRow(i);
+            i -= 1;
+        }
+     }
+     
+      /**
+      * Metodo que vacia la tabla de la vista catalogo y la vuelve a llenar los datos que coincidan con el jtf_buscar.
+      * Por cada tecla escrita en el jtf_buscar se repetira la accion. 
+      */
+     public void jb_buscar_keypresseed(){
+         String sql = "SELECT * FROM sucursal where calle like '"+viewSucursal.jtf_buscar.getText()+"%' OR colonia like '"+viewSucursal.jtf_buscar.getText()+"%'";
+         modelSucursal.setSentencia(sql);
+         limpiar();
+         modelSucursal.llenartabla();
+         viewSucursal.jt_sucursales.setModel(modelSucursal.getT_sucursal());
+     }
 }
