de94c5a59be3734389d7c4c02c5e9975e82fa441
MarioAlbertoNL1799 <37378931+MarioAlbertoNL1799@users.noreply.github.com>
2018-12-04 19:21:50 -0600

    El controlador Compra al seleccionar datos de la tabla es mostrado en los campos.

--- src/controllers/controllerCompras.java
+++ src/controllers/controllerCompras.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package controllers;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import models.modelCompras;
 import views.viewCompra;
 /**
@@ -17,6 +19,13 @@ public class controllerCompras {
     public controllerCompras(modelCompras modelCompras, viewCompra viewCompra) {
         this.modelCompras = modelCompras;
         this.viewCompra = viewCompra;
+        viewCompra.jb_nueva_compra.addMouseListener(moul);
+        viewCompra.jb_modificar_compra.addMouseListener(moul);
+        viewCompra.jb_guardar_compra.addMouseListener(moul);
+        viewCompra.jb_eliminar_compra.addMouseListener(moul);
+        viewCompra.jb_cancelar_compra.addMouseListener(moul);
+        viewCompra.jt_compra.addMouseListener(moul);
+        viewCompra.jt_proveedor.addMouseListener(moul);
         viewCompra.setVisible(true);
         modelCompras.llenarprov();
         viewCompra.jt_proveedor.setModel(modelCompras.getTable_prov());
@@ -31,7 +40,45 @@ public class controllerCompras {
     viewCompra.jcb_idsucursal.removeAllItems();
     modelCompras.sucursales_combobox();
     for(int x = 0; x < modelCompras.getSucursales().size(); x++){
-        viewCompra.jcb_idsucursal.addItem((String) modelCompras.getSucursales().get(x));
-    }
+        viewCompra.jcb_idsucursal.addItem((String) modelCompras.getSucursales().get(x));    }
+};
+    
+    MouseListener moul = new MouseListener() {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            if(e.getSource()== viewCompra.jt_compra)
+                jt_compras_mouseClicked();
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {}
+
+        @Override
+        public void mouseReleased(MouseEvent e) {}
+
+        @Override
+        public void mouseEntered(MouseEvent e) {    }
+
+        @Override
+        public void mouseExited(MouseEvent e) {        }
     };
+    
+    public void jt_compras_mouseClicked(){
+        int linea;
+        String fecha;
+          linea = viewCompra.jt_compra.getSelectedRow();
+          modelCompras.setNo_factura((String) viewCompra.jt_compra.getValueAt(linea, 0));
+          modelCompras.datos_compra();
+          System.out.println(modelCompras.getNo_factura());
+          viewCompra.jtf_nofactura.setText(modelCompras.getNo_factura());
+          viewCompra.jtf_rfc_proveedor.setText(modelCompras.getRfc_prov());
+          viewCompra.jtf_total.setText(String.valueOf(modelCompras.getTotal()));
+          fecha = modelCompras.getFecha();
+          viewCompra.jtf_anio.setText(fecha.substring(0, 4));
+          viewCompra.jtf_mes.setText(fecha.substring(5,7));
+          viewCompra.jtf_dia.setText(fecha.substring(8,fecha.length()));
+          viewCompra.jcb_estado_factura.setSelectedItem(modelCompras.getEstado_factura());
+          viewCompra.jcb_forma_pago.setSelectedItem(modelCompras.getForma_pago());
+          viewCompra.jcb_idsucursal.setSelectedIndex(modelCompras.getSucursal()-1);
     }
+}
