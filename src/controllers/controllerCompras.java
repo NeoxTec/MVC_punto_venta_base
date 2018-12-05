ea02d608e219d083372a94e8caa99d004aac3cf6
MarioAlbertoNL1799 <37378931+MarioAlbertoNL1799@users.noreply.github.com>
2018-12-04 22:55:32 -0600

    Los metodos guardar e insertar ahora pueden ser utilizados. Los botones tienen interacciones acorde su uso.

--- src/controllers/controllerCompras.java
+++ src/controllers/controllerCompras.java
@@ -6,6 +6,7 @@
 package controllers;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.JOptionPane;
 import models.modelCompras;
 import views.viewCompra;
 /**
@@ -42,6 +43,8 @@ public class controllerCompras {
     modelCompras.llenarcompras();
     viewCompra.jt_compra.setModel(modelCompras.getTable_compra());
     viewCompra.jcb_idsucursal.removeAllItems();
+    viewCompra.jb_agregar_detalle.setEnabled(false);
+    viewCompra.jb_modificar_compra.setEnabled(false);
     modelCompras.sucursales_combobox();
     for(int x = 0; x < modelCompras.getSucursales().size(); x++){
         viewCompra.jcb_idsucursal.addItem((String) modelCompras.getSucursales().get(x));    }
@@ -52,12 +55,18 @@ public class controllerCompras {
         public void mouseClicked(MouseEvent e) {
             if(e.getSource()== viewCompra.jt_compra)
                 jt_compras_mouseClicked();
+            else if(e.getSource()==viewCompra.jt_proveedor)
+                jt_proveedor_mouseClicked();
             else if (e.getSource()==viewCompra.jb_nueva_compra)
                 jb_nueva_mouseClicked();
             else if(e.getSource()==viewCompra.jb_cancelar_compra)
                 jb_cancelar_mouseClicked();
             else if(e.getSource()==viewCompra.jb_modificar_compra)
                 jb_modificar_mouseClicked();
+            else if(e.getSource()==viewCompra.jb_eliminar_compra)
+                jb_eliminar_mouseClicked();
+            else if(e.getSource()==viewCompra.jb_guardar_compra)
+                jb_guardar_mouseClicked();
         }
 
         @Override
@@ -90,29 +99,93 @@ public class controllerCompras {
           viewCompra.jcb_estado_factura.setSelectedItem(modelCompras.getEstado_factura());
           viewCompra.jcb_forma_pago.setSelectedItem(modelCompras.getForma_pago());
           viewCompra.jcb_idsucursal.setSelectedIndex(modelCompras.getSucursal()-1);
+          viewCompra.jb_agregar_detalle.setEnabled(true);
+          viewCompra.jb_modificar_compra.setEnabled(true);
     }
 
+    public void jt_proveedor_mouseClicked(){
+        try{
+            viewCompra.jtf_rfc_proveedor.setText(null);
+            int linea;
+            linea = viewCompra.jt_proveedor.getSelectedRow();
+            modelCompras.setRfc_prov((String) viewCompra.jt_proveedor.getValueAt(linea, 0));
+            System.out.println(modelCompras.getRfc_prov());
+            viewCompra.jtf_rfc_proveedor.setText(modelCompras.getRfc_prov());
+        }catch(Exception err){}
+    }
+    
     public void jb_nueva_mouseClicked(){
+        viewCompra.jb_agregar_detalle.setEnabled(false);
+        viewCompra.jb_modificar_compra.setEnabled(false);
+        modelCompras.setValidar(true);
         campos_vacios();
         inicio_b(false);
         acciones_b(true);
+        jt_proveedor_mouseClicked();
     }
     
     public void jb_cancelar_mouseClicked(){
             campos_vacios();
             inicio_b(true);
             acciones_b(false);
+            viewCompra.jb_modificar_compra.setEnabled(false);
     }
     
     public void jb_modificar_mouseClicked(){
+            viewCompra.jb_agregar_detalle.setEnabled(false);
+            modelCompras.setValidar(false);
+            viewCompra.jb_modificar_compra.setEnabled(false);
             inicio_b(false);
             acciones_b(true);
             viewCompra.jtf_nofactura.setEditable(false);
+            viewCompra.jcb_idsucursal.setEnabled(false);
+            viewCompra.jtf_anio.setEditable(false);
+            viewCompra.jtf_mes.setEditable(false);
+            viewCompra.jtf_dia.setEditable(false);
     }
+    
+    public void jb_guardar_mouseClicked(){
+            String fecha;
+            if (viewCompra.jtf_total.getText().isEmpty())
+                modelCompras.setTotal(0);
+            if (viewCompra.jtf_anio.getText().isEmpty() || viewCompra.jtf_dia.getText().isEmpty() || viewCompra.jtf_mes.getText().isEmpty() || viewCompra.jtf_nofactura.getText().isEmpty() || viewCompra.jtf_rfc_proveedor.getText().isEmpty())
+                JOptionPane.showMessageDialog(null, "Ingrese datos para guardar cambios");
+            else{
+                fecha = (viewCompra.jtf_anio.getText()+'-'+viewCompra.jtf_mes.getText()+'-'+viewCompra.jtf_dia.getText());
+                System.out.println(fecha);
+                modelCompras.setEstado_factura(viewCompra.jcb_estado_factura.getSelectedItem());
+                modelCompras.setFecha(fecha);
+                modelCompras.setForma_pago(viewCompra.jcb_forma_pago.getSelectedItem());
+                modelCompras.setNo_factura(viewCompra.jtf_nofactura.getText());
+                modelCompras.setRfc_prov(viewCompra.jtf_rfc_proveedor.getText());
+                modelCompras.setSucursal(viewCompra.jcb_idsucursal.getSelectedIndex()+1);
+             if (modelCompras.isValidar()== true)
+                 modelCompras.insertar();
+             else
+                 modelCompras.modificar();
+            }
+            limpiar_tabla();
+            campos_vacios();
+            init();
+    }
+    
+    public void jb_eliminar_mouseClicked(){
+            viewCompra.jb_agregar_detalle.setEnabled(false);
+            modelCompras.borrar();
+            limpiar_tabla();
+            campos_vacios();
+            init();
+    }
+    
+  public void limpiar_tabla(){
+          for (int i = 0; i < viewCompra.jt_compra.getRowCount(); i++) {
+            modelCompras.getTable_compra().removeRow(i);
+            i -= 1;
+        }
+     }
     
     public void inicio_b(boolean r){
           viewCompra.jb_nueva_compra.setEnabled(r);
-          viewCompra.jb_modificar_compra.setEnabled(r);
           viewCompra.jb_eliminar_compra.setEnabled(r);
     }
     public void acciones_b(boolean s){
@@ -125,7 +198,7 @@ public class controllerCompras {
             viewCompra.jtf_nofactura.setEditable(s);
             viewCompra.jb_guardar_compra.setEnabled(s);
             viewCompra.jb_cancelar_compra.setEnabled(s);
-            viewCompra.jt_proveedor.setRowSelectionAllowed(s);
+            viewCompra.jt_proveedor.setVisible(s);
     }
     
     public void campos_vacios(){
@@ -135,5 +208,8 @@ public class controllerCompras {
          viewCompra.jtf_nofactura.setText(null);
          viewCompra.jtf_rfc_proveedor.setText(null);
          viewCompra.jtf_total.setText(null);
+         viewCompra.jcb_estado_factura.setSelectedIndex(0);
+         viewCompra.jcb_forma_pago.setSelectedIndex(0);
+         viewCompra.jcb_idsucursal.setSelectedIndex(0);
     }
 }
